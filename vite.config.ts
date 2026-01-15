aa151fad903b3a52916c05154bed0630e9a299d7
nnadya <n.d.niki2012@gmail.com>
2026-01-15 23:23:08 +0300

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+console.log('Vite config is being loaded!')
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/c300': 'http://showroom.eis24.me'
+    },
+  },
 })
+
+/* 
+prev version
+
+proxy: {
+  "/api": {
+    target: "http://showroom.eis24.me",
+      changeOrigin: true,
+      secure: false,
+    },
+  },
+},
+
+proxy: {
+      '/c300': {
+        target: "https://showroom.eis24.me",
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/c300/, ''),
+      },
+    },
+*/
