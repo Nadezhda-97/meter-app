30f15bb535d97862088d5fc953e7f59c16b41f48
nnadya <n.d.niki2012@gmail.com>
2026-01-15 23:16:27 +0300

    update RootStore.ts

--- src/models/RootStore.ts
+++ src/models/RootStore.ts
@@ -1,1 +1,23 @@
 // корневой store
+
+import { types } from "mobx-state-tree";
+import type { Instance } from "mobx-state-tree";
+import { MeterStore } from "./MeterStore";
+
+export const RootStore = types.model("RootStore", {
+  meterStore: MeterStore,
+});
+
+// Создаём экземпляр
+export const createRootStore = () => RootStore.create({
+  meterStore: {
+    meters: [],
+    count: 0,
+    loading: false,
+    offset: 0,
+    limit: 20,
+  },
+});
+
+// Тип для использования в компонентах
+export type RootStoreType = Instance<typeof RootStore>;
