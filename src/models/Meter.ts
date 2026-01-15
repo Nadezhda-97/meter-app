068df7097a8877b0abee5c5cd784227159ebf597
nnadya <n.d.niki2012@gmail.com>
2026-01-15 23:15:04 +0300

    update Meter.ts

--- src/models/Meter.ts
+++ src/models/Meter.ts
@@ -1,1 +1,17 @@
 // модель одного счётчика
+
+import { types } from "mobx-state-tree";
+import type { Instance } from "mobx-state-tree";
+
+export const Meter = types.model("Meter", {
+    id: types.identifier,
+    _type: types.string, // ColdWaterAreaMeter или HotWaterAreaMeter
+    installation_date: types.string,
+    is_automatic: types.boolean,
+    initial_values: types.array(types.number),
+    description: types.string,
+});
+
+export type MeterInstance = Instance<typeof Meter>;
+
+// позже - добавить адрес
