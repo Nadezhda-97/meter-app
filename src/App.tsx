79f6abb7da55ee580a0dec49c5386ef227bb72a9
nnadya <n.d.niki2012@gmail.com>
2026-01-16 16:45:09 +0300

    update rootStore

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { RootStoreContext } from "./models/RootStoreContext";
 import { createRootStore } from "./models/RootStore";
 import { MeterTable } from "./components/MeterTable";
+import { useState } from "react";
 // import './App.css'
 
 function App() {
-  const rootStore = createRootStore();
+  //const rootStore = createRootStore();
+  const [rootStore] = useState(() => createRootStore());
+
   return (
       <div>
         <h1>Список счётчиков</h1>
