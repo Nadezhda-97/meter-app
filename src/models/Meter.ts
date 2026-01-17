153aa9a95c6f8fb422638ec6e8cdc30e1b982d3c
nnadya <n.d.niki2012@gmail.com>
2026-01-17 22:31:50 +0300

    add areaId

--- src/models/Meter.ts
+++ src/models/Meter.ts
@@ -6,6 +6,7 @@ import type { Instance } from "mobx-state-tree";
 export const Meter = types.model("Meter", {
     id: types.identifier,
     _type: types.string, // ColdWaterAreaMeter или HotWaterAreaMeter
+    areaId: types.string,
     installation_date: types.string,
     is_automatic: types.optional(types.boolean, false), //types.maybeNull(types.boolean),
     initial_values: types.array(types.number),
