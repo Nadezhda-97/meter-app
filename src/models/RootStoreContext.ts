0585f8d593969ffd0892f7022f3c3d969cf3add5
nnadya <n.d.niki2012@gmail.com>
2026-01-17 22:39:53 +0300

    move file to 'stores' folder

--- src/models/RootStoreContext.ts
+++
@@ -1,6 +0,0 @@
-// провайдер
-
-import { createContext } from "react";
-import type { RootStoreType } from "./RootStore";
-
-export const RootStoreContext = createContext<RootStoreType | null>(null);
