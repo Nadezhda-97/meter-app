5eb98dcc47e9eff76ce1c4989426bd52da1c6c33
nnadya <n.d.niki2012@gmail.com>
2026-01-18 18:25:36 +0300

    fix type

--- src/models/Area.ts
+++ src/models/Area.ts
@@ -3,7 +3,7 @@
 import { types } from "mobx-state-tree";
 
 export const Area = types.model("Area", {
-  id: types.identifierNumber,
+  id: types.identifier, //types.identifierNumber,
   street: types.string,
   house: types.string,
   apartment: types.maybeNull(types.string),
