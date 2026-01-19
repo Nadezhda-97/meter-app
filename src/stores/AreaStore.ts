7aba0597ad4731af09c1d8380c9d30d7995b6b94
nnadya <n.d.niki2012@gmail.com>
2026-01-19 16:59:24 +0300

    update AreaStore.ts

--- src/stores/AreaStore.ts
+++ src/stores/AreaStore.ts
@@ -3,15 +3,38 @@
 import { types, flow } from "mobx-state-tree";
 import { Area } from "../models/Area";
 
-interface AreaData {
+interface AreaApiData {
   id: string;
-  street: string;
-  house: string;
-  apartment: string | null;
+  house?: {
+    id: string;
+    address?: string;
+  };
+  number?: number;
+  str_number?: string;
+  str_number_full?: string;
 }
 
 interface AreaResponse {
-  results: AreaData[];
+  results: AreaApiData[];
+}
+
+const mapApiArea = (area: AreaApiData) => {
+  const address = area.house?.address ?? "";
+
+  // парсинг
+  const [, streetPart, housePart] = address.match(/ул\s([^,]+),\sд\s(\d+)/) ?? [];
+
+  const apartment =
+    area.str_number ??
+    area.str_number_full?.replace(/^кв\.\s*/i, "") ??
+    null;
+
+  return {
+    id: area.id,
+    street: streetPart ?? null,
+    house: housePart ?? null,
+    apartment,
+  };
 }
 
 export const AreaStore = types
@@ -31,22 +54,26 @@ export const AreaStore = types
       self.loading = true;
 
       try {
-        const response: Response = yield fetch(
+        /* const response: Response = yield fetch(
           `/c300/api/v4/test/areas/?id__in=${idsToFetch.join(",")}`
+        ); */
+
+        const query = idsToFetch.map((id) => `id__in=${id}`).join("&");
+
+        const response: Response = yield fetch(
+          `/c300/api/v4/test/areas/?${query}`
         );
 
         if (!response.ok) return;
 
         const data = (yield response.json()) as AreaResponse;
 
+        console.log('address', data); // отладка
+
         // кладём в map
-        data.results.forEach((area: AreaData) => {
-          self.areas.set(String(area.id), {
-            id: area.id,
-            street: area.street,
-            house: area.house,
-            apartment: area.apartment,
-          });
+        data.results.forEach((area) => {
+          const mapped = mapApiArea(area);
+          self.areas.set(mapped.id, mapped);
         });
       } finally {
         self.loading = false;
