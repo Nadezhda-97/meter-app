838a640b72f5de5856dfafd91249b15080f44584
nnadya <n.d.niki2012@gmail.com>
2026-01-20 21:16:53 +0300

    create PageLayout.ts

---
+++ src/styles/PageLayout.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Page = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  justify-content: center; 
+
+  padding: 32px;
+  gap: 16px;
+
+  box-sizing: border-box;
+`;
+
+export const PageTitle = styled.h1`
+  margin: 0;
+  font-size: 20px;
+  font-weight: 600;
+`;
