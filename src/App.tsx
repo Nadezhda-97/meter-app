452fb93b8f9bd20c4e9d1765bfb79fe6072a72a2
nnadya <n.d.niki2012@gmail.com>
2026-01-21 16:48:30 +0300

    update after 'prettier' check

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import { createRootStore } from "./stores/RootStore";
-import { RootStoreContext } from "./stores/RootStoreContext";
+import { createRootStore } from './stores/RootStore';
+import { RootStoreContext } from './stores/RootStoreContext';
 
-import { MeterTable } from "./components/MeterTable";
-import { GlobalStyles } from "./styles/GlobalStyles";
-import { Page, PageTitle } from "./styles/PageLayout";
+import { MeterTable } from './components/MeterTable';
+import { GlobalStyles } from './styles/GlobalStyles';
+import { Page, PageTitle } from './styles/PageLayout';
 
 function App() {
   const [rootStore] = useState(() => createRootStore());
 
   return (
-      <>
-        <GlobalStyles />
-        <RootStoreContext.Provider value={rootStore}>
-          <Page>
-            <PageTitle>Список счётчиков</PageTitle>
-            <MeterTable />
-          </Page>
-        </RootStoreContext.Provider>
-      </>
-  )
+    <>
+      <GlobalStyles />
+      <RootStoreContext.Provider value={rootStore}>
+        <Page>
+          <PageTitle>Список счётчиков</PageTitle>
+          <MeterTable />
+        </Page>
+      </RootStoreContext.Provider>
+    </>
+  );
 }
 
-export default App
+export default App;
