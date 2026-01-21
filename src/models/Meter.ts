85a55d6776b16f69ab4ee0ad700b078a148d2b81
nnadya <n.d.niki2012@gmail.com>
2026-01-21 16:52:26 +0300

    update after 'prettier' check

--- src/models/Meter.ts
+++ src/models/Meter.ts
@@ -1,14 +1,14 @@
-import { types } from "mobx-state-tree";
-import type { Instance } from "mobx-state-tree";
+import { types } from 'mobx-state-tree';
+import type { Instance } from 'mobx-state-tree';
 
-export const Meter = types.model("Meter", {
-    id: types.identifier,
-    _type: types.string,
-    areaId: types.string,
-    installation_date: types.string,
-    is_automatic: types.optional(types.boolean, false),
-    initial_values: types.array(types.number),
-    description: types.optional(types.string, ""),
+export const Meter = types.model('Meter', {
+  id: types.identifier,
+  _type: types.string,
+  areaId: types.string,
+  installation_date: types.string,
+  is_automatic: types.optional(types.boolean, false),
+  initial_values: types.array(types.number),
+  description: types.optional(types.string, ''),
 });
 
 export type MeterInstance = Instance<typeof Meter>;
