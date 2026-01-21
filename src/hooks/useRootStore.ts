4583e81fa55badf99ba50c2ac93e9e64eb30d4fc
nnadya <n.d.niki2012@gmail.com>
2026-01-21 17:07:46 +0300

    update after 'prettier' check

--- src/hooks/useRootStore.ts
+++ src/hooks/useRootStore.ts
@@ -1,10 +1,10 @@
-import { useContext } from "react";
-import { RootStoreContext } from "../stores/RootStoreContext";
+import { useContext } from 'react';
+import { RootStoreContext } from '../stores/RootStoreContext';
 
 export const useRootStore = () => {
   const context = useContext(RootStoreContext);
   if (!context) {
-    throw new Error("useRootStore must be used within RootStoreProvider");
+    throw new Error('useRootStore must be used within RootStoreProvider');
   }
   return context;
 };
