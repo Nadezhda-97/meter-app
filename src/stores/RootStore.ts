35f7bfcc32540c70cdfbf433a50662310aa4a031
nnadya <n.d.niki2012@gmail.com>
2026-01-21 16:51:17 +0300

    update after 'prettier' check

--- src/stores/RootStore.ts
+++ src/stores/RootStore.ts
@@ -1,25 +1,26 @@
-import { types } from "mobx-state-tree";
-import type { Instance } from "mobx-state-tree";
-import { MeterStore } from "./MeterStore";
-import { AreaStore } from "./AreaStore";
+import { types } from 'mobx-state-tree';
+import type { Instance } from 'mobx-state-tree';
+import { MeterStore } from './MeterStore';
+import { AreaStore } from './AreaStore';
 
-export const RootStore = types.model("RootStore", {
+export const RootStore = types.model('RootStore', {
   meterStore: MeterStore,
   areaStore: AreaStore,
 });
 
-export const createRootStore = () => RootStore.create({
-  meterStore: {
-    meters: [],
-    count: 0,
-    loading: false,
-    offset: 0,
-    limit: 20,
-  },
-  areaStore: {
-    areas: {},
-    loading: false,
-  },
-});
+export const createRootStore = () =>
+  RootStore.create({
+    meterStore: {
+      meters: [],
+      count: 0,
+      loading: false,
+      offset: 0,
+      limit: 20,
+    },
+    areaStore: {
+      areas: {},
+      loading: false,
+    },
+  });
 
 export type RootStoreType = Instance<typeof RootStore>;
